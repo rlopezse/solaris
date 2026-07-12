69b10ad38243f09c1ef16226acdbfe30a369646e
rlopezse <rza.lopez.s@gmail.com>
2026-07-12 18:20:46 -0400

    added: login con google oauth

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { authController } from '../controllers/auth.controller'
 import { validate } from '../middlewares/validate.middleware'
-import { registerSchema, loginSchema } from '../schemas/auth.schema'
+import { registerSchema, loginSchema, googleLoginSchema } from '../schemas/auth.schema'
 
 const router = Router()
 router.post('/register', validate(registerSchema), authController.register)
+router.post('/login-google', validate(googleLoginSchema), authController.loginGoogle)
 router.post('/login', validate(loginSchema), authController.login)
 
 export default router
--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -11,5 +11,23 @@ export const loginSchema = z.object({
   password: z.string().min(1),
 })
 
-export type RegisterDto = z.infer<typeof registerSchema>
-export type LoginDto = z.infer<typeof loginSchema>
+export const googleLoginSchema = z.object({
+  idToken: z.string().min(1),
+})
+
+export const googleIdTokenPayloadSchema = z.object({
+  iss: z.url(), 
+  sub: z.string(),
+  aud: z.string(), 
+  azp: z.string().optional(), 
+  email: z.email(),
+  email_verified: z.boolean(),
+  at_hash: z.string().optional(),
+  name: z.string(),
+  picture: z.url(),
+  given_name: z.string(),
+  family_name: z.string(),
+  locale: z.string().optional(),
+  iat: z.number(),
+  exp: z.number(),
+});
